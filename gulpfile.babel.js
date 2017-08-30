7b6d696d3cbe183a079fce59439b2effcbb01868
jayMcoder <jayaram.h4u@gmail.com>
2017-08-30 21:06:00 +0100

    add comment and update package.json

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,14 @@
+/*
+* Gulp configuration for babel task
+*/
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 
+/* Default task loads js from src
+* folder and outputs to dist/gulp folder
+*/
 gulp.task('default', ()=>{
   return gulp.src('src/*.js')
   .pipe(babel())
-  .pipe(gulp.dest('dist'));
+  .pipe(gulp.dest('dist/gulp'));
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
+/*
+* Webpack configuration for babel
+* Uses babel-loader
+* Entry point set to main.js 
+* Webpack traverses and creates
+* single bundle.js file in 
+* dist/webpack folder
+*/
+
 module.exports = {
   entry: ['./src/main.js'],
   output: {
-    path: 'dist',
+    path: 'dist/webpack',
     filename: 'bundle.js'
   },
   module:{
